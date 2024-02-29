6286e6e14754e692a235cc79a6bb13dfa50085dc
kwangjo93 <125628009+kwangjo93@users.noreply.github.com>
2024-02-29 21:33:51 +0900

    Set DogFirendTableView

---
+++ DogFriendList-MVVM-Protocol-Input-Output-/View/Controller/DogFriendListTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  DogFriendListTableViewCell.swift
+//  DogFriendList-MVVM-Protocol-Input-Output-
+//
+//  Created by 천광조 on 2/29/24.
+//
+
+import UIKit
+
+class DogFriendListTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
